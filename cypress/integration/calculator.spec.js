54c0ee4770c0e3ce5c0cbc2d9389e69993bd1f24
Carlos Zubillaga <carloszubillaga@Carloss-MacBook-Pro.local>
2021-10-12 19:24:24 +0100

    handles decimals test passes

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,41 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update display of running total', () => {
+    cy.get('#number4').click();
+    cy.get('#running-total').should('contain', '4');
+  })
+
+  it('should update the display with the result of the operation', () => {
+    cy.get('#number8').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').should('contain', '72');
+  });
+
+  it('should be able to chain multiple operations together', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').should('contain', '5');
+  });
+
+  it('should handle decimals', () => {
+    cy.get('#number3').click();
+    cy.get('#decimal').click();
+    cy.get('#number8').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    // cy.get('#decimal').click();
+    cy.get('#number6').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').should('contain', '98.8');
+  });
 })
